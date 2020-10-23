ac7ab7fe45c196f045fa50bd5907b70b9a103090
alberth270 <aba.27.0309@gmail.com>
2020-10-23 15:04:13 -0500

    vesrion-dockerizada

--- src/main/java/com/everis/proyect/controller/ReniecController.java
+++ src/main/java/com/everis/proyect/controller/ReniecController.java
@@ -4,13 +4,11 @@ import io.reactivex.Single;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
 @RestController
 @RequestMapping("/external/reniec")
 public class ReniecController {
